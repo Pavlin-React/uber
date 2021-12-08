871aab1e38d62bf6a0edd8a36c6ac704c29d994f
Pavlin Ivanov <pavlinivanov9@gmail.com>
2021-12-08 15:53:02 +0200

    react-native-google-places-autocomplete

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,17 +2,36 @@ import React from "react";
 import { Text, SafeAreaView, View, Image } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from '@env'
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={tw`h-full bg-white`}>
-      <View style={{padding: 10}} >
+      <View style={{ padding: 10 }}>
         <Image
-          style={{ width: 100, height: 100}}
-          source={require('../assets/uber.png')}
-          resizeMode='contain'
+          style={{ width: 100, height: 100 }}
+          source={require("../assets/uber.png")}
+          resizeMode="contain"
         />
-        <NavOptions/>
+        <GooglePlacesAutocomplete
+          styles={{
+            container: {
+              flex: 0
+            },
+            textInput: {
+              fontSize: 20
+            }
+          }}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en',
+          }}
+          placeholder='Where From?'
+          nearbyPlacesAPI='GooglePlacesSearch'
+          debounce={400}
+        />
+        <NavOptions />
       </View>
     </SafeAreaView>
   );
